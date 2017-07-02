0f481a2c3d8d681a95a93ddf7ceb97a413e78496
andres90sosa <andres90sosa@gmail.com>
2017-07-02 19:10:33 -0300

    Lección 1

---
+++ ProjectDemo/Controllers/VideoJuegosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectDemo.Models;
+
+namespace ProjectDemo.Controllers
+{
+    [Authorize]
+    public class VideoJuegosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /VideoJuegos/
+        public ActionResult Index()
+        {
+            return View(db.VideoJuegos.ToList());
+        }
+
+        // GET: /VideoJuegos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VideoJuego videojuego = db.VideoJuegos.Find(id);
+            if (videojuego == null)
+            {
+                return HttpNotFound();
+            }
+            return View(videojuego);
+        }
+
+        // GET: /VideoJuegos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /VideoJuegos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Nombre,Descripcion,ImagenUrl")] VideoJuego videojuego)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VideoJuegos.Add(videojuego);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(videojuego);
+        }
+
+        // GET: /VideoJuegos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VideoJuego videojuego = db.VideoJuegos.Find(id);
+            if (videojuego == null)
+            {
+                return HttpNotFound();
+            }
+            return View(videojuego);
+        }
+
+        // POST: /VideoJuegos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Nombre,Descripcion,ImagenUrl")] VideoJuego videojuego)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(videojuego).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(videojuego);
+        }
+
+        // GET: /VideoJuegos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VideoJuego videojuego = db.VideoJuegos.Find(id);
+            if (videojuego == null)
+            {
+                return HttpNotFound();
+            }
+            return View(videojuego);
+        }
+
+        // POST: /VideoJuegos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VideoJuego videojuego = db.VideoJuegos.Find(id);
+            db.VideoJuegos.Remove(videojuego);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectDemo/Models/VideoJuego.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectDemo.Models
+{
+    public class VideoJuego
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
